44cdae04e8de383cdc7ff0cb52e7bd90beec2291
Nachiket V Sarode <nachiketvsarode@gmail.com>
2024-11-10 21:22:42 -0500

    learning folder structure

--- main.go
+++ main.go
@@ -1,7 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"mylearning/myutil"
+)
 
 func main() {
-	fmt.Println("Learn go language by hellp world")
+	fmt.Println("Learn go language by hello world")
+
+	myutil.PrintMessage("Hello World")
+
 }
