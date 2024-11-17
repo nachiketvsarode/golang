436409d0e3226b503f53f558fe67d1138b578c71
Nachiket V Sarode <nachiketvsarode@gmail.com>
2024-11-16 23:08:39 -0500

    learning time format in golang

--- 16.time_date_function/main.go
+++ 16.time_date_function/main.go
@@ -1,10 +1,26 @@
 package main
 
+import (
+	"fmt"
+	"time"
+)
+
 //golang has fixed time and date format in 24 hour format
 // eg. 2006-01-02 15:04:05
-//Y : 2006
-//
+//YYYY : 2006
+//MM : Month
+//DD : date
+//hh: hours in 24 hours format
+//mm: minutes
+//ss: seconds
 
 func main() {
+
+	currentTime := time.Now()
+	fmt.Println("Current time: ", currentTime)
+	fmt.Printf("Type of CurrentTime %T\n", currentTime)
+
+	formatted := currentTime.Format("dd-mm-yyyy")
+	fmt.Println("Formatted time: ", formatted)
 
 }
