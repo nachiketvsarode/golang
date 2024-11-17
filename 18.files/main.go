dc18554a66ad74331358764d15287c7fa9e69e3c
Nachiket V Sarode <nachiketvsarode@gmail.com>
2024-11-17 16:06:38 -0500

    commenting the codes untill now

--- 18.files/main.go
+++ 18.files/main.go
@@ -1,33 +1,32 @@
 package main
 
-import (
-	"fmt"
-	"io"
-	"os"
-)
-
 func main() {
-	file, err := os.Create("example.txt")
+	/*
+		file, err := os.Create("example.txt")
 
-	if err != nil {
-		fmt.Println("Error while creating file: ", err)
-		return
-	}
-	defer file.Close()
+		if err != nil {
+			fmt.Println("Error while creating file: ", err)
+			return
+		}
+		defer file.Close()
+	*/
+	/*
+		once func main() then close will be run, this function is important in many cases when you are done working with file.
+		when you oopen a file using function like os.create, os.Open orthers you are utilizing system reosurces to interact with that file.
+		failing to close could leak resources & cause iissues like running out of descriptors.
+		the Close() function is responsible for releasing  those resources.
+	*/
+	/*
+		content := "hello world by prince"
+		byte, errors := io.WriteString(file, content+"\n")
+		fmt.Fprintln(os.Stdout, []any{"byte written:", byte}...)
+		if errors != nil {
+			fmt.Println("Error while writing file:", errors)
+			return
+		}
 
-	//once func main() then close will be run, this function is important in many cases when you are done working with file.
-	//when you oopen a file using function like os.create, os.Open orthers you are utilizing system reosurces to interact with that file.
-	//failing to close could leak resources & cause iissues like running out of descriptors.
-	// the Close() function is responsible for releasing  those resources.
+		fmt.Println("successfully created file")
 
-	content := "hello world by prince"
-	byte, errors := io.WriteString(file, content+"\n")
-	fmt.Fprintln(os.Stdout, []any{"byte written:", byte}...)
-	if errors != nil {
-		fmt.Println("Error while writing file:", errors)
-		return
-	}
-
-	fmt.Println("successfully created file")
+	*/
 
 }
