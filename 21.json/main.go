f53479f413443c9d8bd32a6c85e2a8d511c2844c
Nachiket V Sarode <nachiketvsarode@gmail.com>
2024-11-17 18:53:11 -0500

    learning marshalling in golang

---
+++ 21.json/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type Person struct {
+	Name    string `json:"name"`
+	Age     int    `json:"age"`
+	IsAdult bool   `json:"is_Adult"`
+}
+
+func main() {
+	fmt.Println("we are learning JSON")
+	person := Person{Name: "nachiket", Age: 34, IsAdult: true}
+	fmt.Println("person Data is:", person)
+
+	//convert person into JSON Encoding (Marshalling)
+
+	jsonData, err := json.Marshal(person)
+	if err != nil {
+		fmt.Println("Error marshalling", err)
+		return
+	}
+	fmt.Println("person Data is:", string(jsonData))
+
+}
