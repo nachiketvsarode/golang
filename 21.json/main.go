98a2526208e8746d0d9255ec9b85c6d6ec628626
Nachiket V Sarode <nachiketvsarode@gmail.com>
2024-11-17 18:58:29 -0500

    learning unmarshalling in golang

--- 21.json/main.go
+++ 21.json/main.go
@@ -25,4 +25,14 @@ func main() {
 	}
 	fmt.Println("person Data is:", string(jsonData))
 
+	//decoding unmarshalling data
+
+	var personData Person
+	err = json.Unmarshal(jsonData, &personData)
+	if err != nil {
+		fmt.Println("Error unmarshalling", err)
+		return
+	}
+	fmt.Println("person Data after unmarshalling: ", personData)
+
 }
